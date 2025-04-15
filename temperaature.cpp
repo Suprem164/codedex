201a21d116c6de8fef8b7c814a52761ae816d110
Lawarna Aree <lawarna@2400-1A00-B040.ip6.wlink.com.np>
2025-04-15 22:55:31 +0545

    added temperature.cpp

---
+++ temperaature.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double temp = 87;
+
+    if(temp >= 100)
+    {
+        cout << "You have a fever.";
+    }
+
+    else {
+        cout << "You do not have a fever.";
+    }
+
+    return 0;
+}
